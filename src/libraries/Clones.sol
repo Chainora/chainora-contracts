e388c8962373783eebfcb5eb8a5cee96878d7ef3
Duong <tuanduong152004@gmail.com>
2026-04-05 16:44:55 +0700

    format clones library

--- src/libraries/Clones.sol
+++ src/libraries/Clones.sol
@@ -6,17 +6,8 @@ import {Errors} from "src/libraries/Errors.sol";
 library Clones {
     function clone(address implementation) internal returns (address instance) {
         assembly ("memory-safe") {
-            mstore(
-                0x00,
-                or(
-                    shr(0xe8, shl(0x60, implementation)),
-                    0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000
-                )
-            )
-            mstore(
-                0x20,
-                or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3)
-            )
+            mstore(0x00, or(shr(0xe8, shl(0x60, implementation)), 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000))
+            mstore(0x20, or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3))
             instance := create(0, 0x09, 0x37)
         }
         if (instance == address(0)) revert Errors.CloneCreateFailed();
